0aad4f43255f9adfae169f552b616eaf7f9aa25d
sakshiw02 <90848830+sakshiw02@users.noreply.github.com>
2023-10-25 23:41:05 +0530

    Add files via upload

---
+++ Java_Stdin_Sdtout_I.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Java_Stdin_Stdout_I{
+
+
+public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    for(int i=0;i<3;i++){
+    int a = scan.nextInt();
+    System.out.println(a);
+    }
+}
+}
